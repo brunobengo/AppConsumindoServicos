288b86a2a34990a8ef652ba89d3f779b5bae9a6f
brunobengo <brunobengo@gmail.com>
2019-01-28 23:51:20 -0200

    adição do consumo de api na web e captura de valor específico do json consumido

--- AppConsumindoServicos/ViewController.swift
+++ AppConsumindoServicos/ViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        if let url = URL(string: "https://blockchain.info/ticker"){
+            let tarefa = URLSession.shared.dataTask(with: url) { (dados, requisicao,
+                erro) in
+                
+                if erro == nil {
+                    if let dadosRetorno = dados {
+                        
+                        do{
+                            if let objetoJson = try JSONSerialization.jsonObject(with: dadosRetorno, options: [])
+                                as? [String: Any] {
+//                              print(objetoJson)
+                                if let brl = objetoJson["BRL"] as? [String: Any] {
+//                                    print(brl)
+                                    if let preco = brl["buy"] as? Double {
+                                        print(preco)
+                                        
+                                    }else{
+                                        print("nops work")
+                                    }
+                                }
+                            }
+                        }catch{
+                            print("Erro ao formatar o retorno")
+                        }
+                    }
+                }else{
+                    print("não funcionou")
+                }
+            }
+            tarefa.resume()
+        }
     }
-
-
+    
 }
 
